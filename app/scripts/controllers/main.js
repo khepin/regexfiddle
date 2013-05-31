970e33243a5a4052df2a8541e437edcdfdef5955
khepin <khepin@gmail.com>
2013-05-31 16:58:39 +0800

    more awesomeness

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,17 +25,33 @@ m.controller('MainCtrl', function ($scope, $routeParams) {
     $scope.regex.attributes.flags = 'g';
     $scope.regexResult = function() {
         if (!$scope.regex.attributes.data) {
-            return [];
+            return ;
         }
 
         var data = $scope.regex.attributes.data.split('\n');
-        if (!$scope.regex.attributes.regex) {
-            return data;
+
+        var replace = $scope.regex.attributes.replace || '';
+
+        var spanOpen, spanClose;
+        if (!$scope.regex.attributes.doReplace) {
+            spanOpen = Math.floor(Math.random() * 80000);
+            spanClose = Math.floor(Math.random() * 80000);
+            replace = spanOpen + '$1' + spanClose;
         }
-        var replace = $scope.regex.attributes.replace || '<span class="matched">$1</span>';
 
         for (var i = 0; i < data.length; i++) {
-            data[i] = data[i].replace(new RegExp('(' + $scope.regex.attributes.regex + ')', $scope.regex.attributes.flags), replace).replace(/\n/g, '<br>');
+            if ($scope.regex.attributes.regex) {
+                var reg = new RegExp('(' + $scope.regex.attributes.regex + ')', $scope.regex.attributes.flags);
+                data[i] = data[i].replace(reg, replace);
+            }
+
+            data[i] = _.escape(data[i]).replace(/\s/g, '&nbsp;');
+
+            if (spanOpen) {
+                data[i] = data[i].replace(new RegExp(spanOpen, 'g'), '<span class="matched">');
+                data[i] = data[i].replace(new RegExp(spanClose, 'g'), '</span>');
+            }
+            data[i] = data[i].replace(/\n/g, '<br>');
         }
         return data;
     };
